c7070b98e5332ee4e94c8a09e18f62c5fc8ecac2
scarlettcsung <85265936+scarlettcsung@users.noreply.github.com>
2026-02-09 14:36:43 +0100

    Added data saves

--- R/01-coexpression-analysis.R
+++ R/01-coexpression-analysis.R
@@ -3,7 +3,7 @@ source("R/coexpression-functions.R")
 
 # ================= DATA SETUP =================
 
-path <- "data/k12_counts.csv" # File Path
+path <- "data/BURK_counts.csv" # File Path
 raw_counts <- read.csv(path, header=TRUE, row.names = 1) # Load .csv
 raw_counts <- raw_counts[order(colnames(raw_counts))]
 
@@ -13,15 +13,16 @@ raw_counts <- raw_counts[order(colnames(raw_counts))]
 counts <- filterLowExpression(raw_counts) #Rows: Gene name, Columns: Conditions
 counts_filtered <- counts[rowSums(counts) > 0, colSums(counts) > 0]
 
-k12_coexpr <- coexpr_analysis(counts_filtered,2)
-matrices <- k12_coexpr$matrix
-genepairs <- k12_coexpr$genepairs
+saveRDS(counts_filtered,file = "results/burkholderia_filtered.rds")
 
-plot_all_methods_by_bins(counts_filtered,2)
+burk_coexpr <- coexpr_analysis(counts_filtered,3)
+matrices <- burk_coexpr$matrix
+genepairs <- burk_coexpr$genepairs
 
-saveRDS(matrices,file="results/k12_matrices.rds")
-saveRDS(genepairs,file="results/k12_genepairs.rds")
+saveRDS(matrices,file="results/burkholderia_matrices.rds")
+saveRDS(genepairs,file="results/burkholderia_genepairs.rds")
 
+plot_all_methods_by_bins(counts_filtered,3)
 
 
 
